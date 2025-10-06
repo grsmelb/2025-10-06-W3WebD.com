79806995199339e5f5f8d2ea4d3353ce04595caa
GRS <smartparking131@gmail.com>
2025-10-06 12:55:00 +0530

    1.0.4 Restructuring: uploading main files dist

---
+++ 0Testing/v1.1-test-fernicons-with-responsive-bugs/5FirstProject-IconsCollection/dist/js/utilities/6utility-fs-promise-then-phosphorIcon.js
@@ -0,0 +1,74 @@
+// This is CommonJS module. Coverted to ES Module var require: NodeJS.Require
+let http = require("http");
+let fs = require("fs");
+let path = require('path');
+let directorypath = '/home/phoenix/Documents/phosphor';
+let directoryfileoutput = '/home/fern/Documents/ferntestoutput/fileoutput.json';
+
+let test = false;
+
+
+let svgObjectArray = [];
+
+let svgObjefunctionctList = {
+    name: "",
+    svg: "<svg></svg>"
+};
+
+
+const myPromise1 = new Promise((resolve, reject) => {
+    // Simulate an asynchronous operation
+
+    if (test) console.log('Inside ReadDirFiles function');
+
+    fs.readdir(directorypath, (err, files) => {
+        if (err) {
+            console.error("Error Reading Directory: ", err);
+            return;
+        }
+        console.log('Files in the directory:');
+
+        files.forEach(file => {
+            if (test) console.log("File name: ", file);
+            let filepath = directorypath + "/" + file;
+            let data = fs.readFileSync(filepath, 'utf8');
+            if (test) console.log("file Content: ", data);
+            svgObjefunctionctList.name = file.split('.')[0];
+            svgObjefunctionctList.svg = data;
+            svgObjectArray.push(JSON.parse(JSON.stringify(svgObjefunctionctList)));
+            if (test) console.log('SVG object: ', svgObjectArray);
+
+            // fs.readFile(filepath, 'utf8', (err, data) => {
+            //     if (err) {
+            //         console.log(err);
+            //         return;
+            //     }
+            //     console.log("file Content: ", data);
+            //     svgObjefunctionctList.name = file.split('.')[0];
+            //     svgObjefunctionctList.svg = data;
+            //     svgObjectArray.push(JSON.parse(JSON.stringify(svgObjefunctionctList)));
+            //     console.log('SVG object: ', svgObjectArray);
+
+            // });
+        })
+        resolve(svgObjectArray);
+        if (test) console.log('Total Array after read: ', svgObjectArray)
+    })
+
+});
+myPromise1
+    .then((data) => {
+        let filecontentstring = JSON.stringify(data);
+
+        fs.writeFile(directoryfileoutput, filecontentstring, (err) => {
+            if (err) {
+                console.log("error vvriting: ", err);
+                return;
+            }
+            console.log('File vvritten');
+        })
+    })
+
+    .catch((error) => {
+        console.error("Error: Promise rjected: ", error); // Executed if any promise in the chain is rejected
+    });
